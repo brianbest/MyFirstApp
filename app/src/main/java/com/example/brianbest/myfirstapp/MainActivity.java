b2a514f94848f5449ee5feeeb0ca5563314e88c8
Brian best <brian.brianb64@gmail.com>
2015-06-13 14:45:54 -0300

    finshed

--- app/src/main/java/com/example/brianbest/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/brianbest/myfirstapp/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.brianbest.myfirstapp;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,7 +16,37 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TextView textView = (TextView)findViewById(R.id.mainText);
+        textView.setText("Spencer wrote this code!");
+
+        textView.setTextColor(Color.parseColor("#00FFFF"));
+        textView.setTextSize(30);
+
+        textView.setGravity(1);
+
+
+        TextView secondtextView = (TextView)findViewById(R.id.secondText);
+        secondtextView.setText("Spencer plays minecraft");
+        secondtextView.setY(250);
+
+        secondtextView.setTextColor(Color.parseColor("#0000FF"));
+        secondtextView.setTextSize(30);
+
+        secondtextView.setGravity(1);
+
+        Button clickButton=(Button) findViewById(R.id.button);
+        clickButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = (TextView)findViewById(R.id.mainText);
+                textView.setTextColor(Color.parseColor("#FF0000"));
+            }
+        });
+
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
